642f14e9a232ae7a23c5bbd0f533800feffeb470
Erik Bjäreholt <erik@bjareho.lt>
2023-10-03 12:42:25 +0200

    feat: added automatic fetching of stars

--- generate_readme.py
+++ generate_readme.py
@@ -7,9 +7,18 @@ template = env.get_template("README.md.in")
 with open("data.yaml", "r") as f:
     tables = yaml.safe_load(f)
 
-# print(f"Tables: {tables.keys()}")
+with open("stars.yaml", "r") as f:
+    stars = yaml.safe_load(f)
 
-rendered_template = template.render(tables)
+# combine
+for cat in tables:
+    for row in tables[cat]:
+        print(stars, row["link"])
+        if row["link"] in stars:
+            row["stars"] = stars[row["link"]]["stars"]
+
+
+rendered_template = template.render(tables, stars=stars)
 
 with open("README.md", "w") as f:
     f.write(rendered_template)
